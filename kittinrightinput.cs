3d672d896ae41de21bb4cfe3ff69cb8bfd502f8d
Yuto-x <57879864+Yuto-x@users.noreply.github.com>
2020-01-27 14:46:58 +0900

    Add files via upload

---
+++ kittinrightinput.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kittinrightinput : MonoBehaviour
+{
+    public GameObject kittin;
+
+    //ボタン
+    public GameObject obj1F;    //1階モード
+    public GameObject obj2F;    //2階モード
+
+    GameObject JudgKittin1F;
+    GameObject JudgKittin2F;
+    
+    public void OnClick()
+    {
+        JudgKittin1F = GameObject.FindGameObjectWithTag("1Fkittin");
+        JudgKittin2F = GameObject.FindGameObjectWithTag("2Fkittin");
+        if (JudgKittin1F == null)
+        {
+            if (obj1F.activeSelf == true)
+            {
+                GameObject kit = Instantiate(kittin);
+                kit.tag = "1Fkittin";
+                kit.transform.position = new Vector3(36, 2, 17);
+                kit.transform.Rotate(0.0f, 0.0f, 0.0f);
+            }
+        }else if (JudgKittin2F == null)
+        {
+            if (obj2F.activeSelf == true)
+            {
+                GameObject kit = Instantiate(kittin);
+                kit.tag = "2Fkittin";
+                kit.transform.position = new Vector3(36, 6, 17);
+                kit.transform.Rotate(0.0f, 0.0f, 0.0f);
+            }
+        }
+        else if(JudgKittin != null)
+        {
+            Destroy(JudgKittin);
+            if (obj1F.activeSelf == true)
+            {
+                GameObject kit = Instantiate(kittin);
+                kit.tag = "1Fkittin";
+                kit.transform.position = new Vector3(36, 2, 17);
+                kit.transform.Rotate(0.0f, 0.0f, 0.0f);
+            }
+            else if (obj2F.activeSelf == true)
+            {
+                GameObject kit = Instantiate(kittin);
+                kit.tag = "2Fkittin";
+                kit.transform.position = new Vector3(36, 6, 17);
+                kit.transform.Rotate(0.0f, 0.0f, 0.0f);
+            }
+        }
+    }
+}
